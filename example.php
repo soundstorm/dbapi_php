7f556bba29139a88114a219e4d7a0b822e5a32ef
Luca Schröder <soundstorm@users.noreply.github.com>
2023-12-11 14:53:27 +0100

    Missing semicolon

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 
-include("db.php")
+include("db.php");
 $db = new DeutscheBahn();
 foreach ($db->getStationByName('Hannover Hbf')[0]->getDepatures(10) as $journey) {
 	print($journey->getProduct()."\t".$journey->getLine()."\t".$journey->getDirection()."\n");
