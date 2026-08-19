cf9efd3af04da34998a79bd6e472ac9ce1cd3e74
Luca Schröder <soundstorm@users.noreply.github.com>
2026-08-19 20:29:13 +0200

    New API

--- example.php
+++ example.php
@@ -2,6 +2,6 @@
 
 include("db.php");
 $db = new DeutscheBahn();
-foreach ($db->getStationByName('Hannover Hbf')[0]->getDepatures(10) as $journey) {
+foreach ($db->getStationByName('Hannover Hbf')[0]->getDepartures(10) as $journey) {
 	print($journey->getProductShort()."\t".$journey->getLine()."\t".$journey->getDirection()."\n");
 }
